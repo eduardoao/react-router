3c1f6630ec6d90756cb253a53078e3d7264a668c
Eduardo Alcantara de Oliveira <eduardo_alcantara@hotmail.com>
2019-04-10 11:15:34 -0300

    Bootstrap add

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Main from './Main';
 import "./index.css";
+import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(
     <Main/>, 
